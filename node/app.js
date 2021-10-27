0c2351bd0289512984fb8a4490c58fc58280984f
AlexNoch <44502017+Alexnoch@users.noreply.github.com>
2021-10-27 12:13:24 +0300

    old version

--- node/app.js
+++ node/app.js
@@ -1,19 +1,32 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var app = express();
 app.set('port', process.env.PORT || 3005);
+mongoose.connect('mongodb+srv://alexnoch135:44335522@cluster0.brals.mongodb.net/apiblog?retryWrites=true&w=majority', {useNewUrlParser: true})
+.then(()=> console.log('Mongo DB Подключена'))
+.catch((err)=>{console.log(err,'ОШибка')})
 
-   app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-   });
+
+const ArticlesSvhema = new mongoose.Schema({
+   name:{
+      type:String,
+      required:true,
+   },
+   email:{
+      type:String,
+      required: true,
+   }
+})
+
+
    app.get('/about', function(req, res){
     res.set("Content-Type","application/json");
     res.set("Access-Control-Allow-Origin","*" )
     res.set("Access-Control-Allow-Methods","GET, OPTIONS" )
     res.set("Access-Control-Allow-Headers","Content-Type" )
-    res.type('text/plain');
-    res.status(200);
-    res.send('О Meadowlark Travel');
+    res.status(200)
+    res.send(JSON.stringify({article1:"Стать о человеке"}));
+   
    });
 
 // пользовательская страница 404
@@ -29,8 +42,6 @@ app.use(function(req, res){
     res.status(500);
     res.send('500 — Ошибка сервера');
    });
-
-
 
    app.listen(app.get('port'), function(){
     console.log( 'Express запущен на http://localhost:' +
